198a586b6bad032c5dd334978621ce32b00e352f
Sophie Maquiling <scmaquiling@ucdavis.edu>
2017-01-26 16:24:54 -0800

    update studio2

--- studio2/script.js
+++ studio2/script.js
@@ -3,6 +3,14 @@
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 
-    // all other js here
+    document.mainForm.onsubmit = processForm;
 
-});
+    function processForm() {
+      var userName = document.mainForm.userName.value;
+      var texterRel = document.mainForm.texterRel.value;
+      var texterName = document.mainForm.texterName.value;
+      var results = document.getElementById('results');
+      results.innerHTML = "Your name is " + userName + " and you are texting your " + texterRel + " whose name is " + texterName;
+      return false;
+    } // processForm()
+}); // DOMContentLoaded
