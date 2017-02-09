daf2d3d55435464262a7872dd08ac299f6a1b3de
sopmaq <scmaquiling@ucdavis.edu>
2017-02-09 04:22:08 -0800

    fix images and layout

--- studio3/script.js
+++ studio3/script.js
@@ -8,15 +8,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var description = document.getElementById("description");
 var close = document.getElementById("close");
 var img1 = document.getElementById("img-1");
-var img2 = document.getElementById("img-2");
+var mHeader = document.getElementById("mHeader");
+//var img2 = document.getElementById("img-2");
 
 
 // Close
+
+close.addEventListener('mouseover', function() {
+  close.style.cursor = "pointer";
+})
+
 close.addEventListener('click', function() {
   close.style.cursor = "pointer";
   description.style.transition = "all 1s";
   description.style.opacity = "0";
   description.style.visibility = "hidden";
+  mHeader.style.opacity = "1";
+  mHeader.style.transition = "all 0.7s";
 })
 // Micron
    var micron = document.getElementById("micron");
@@ -30,13 +38,15 @@ close.addEventListener('click', function() {
   })
   micron.addEventListener('click', function() {
     img1.src = "images/micron_line.svg";
-    img2.src = "images/micron.svg";
+  //  img2.src = "images/micron.svg";
     document.getElementById("description-h3").innerHTML = "SAKURA";
     document.getElementById("description-h5").innerHTML = "Pigma Micron 08";
     document.getElementById("description-p").innerHTML = "Quintessential \"designer's pen.\" I like to use this for hand-lettering and illustrations."
     description.style.opacity = "1";
     description.style.visibility = "visible";
     description.style.transition = "all 1.5s";
+    mHeader.style.opacity = "0";
+    mHeader.style.transition = "all 0.7s";
   })
 
 
@@ -52,13 +62,15 @@ close.addEventListener('click', function() {
      })
      muji.addEventListener('click', function() {
        img1.src = "images/muji_line.svg";
-       img2.src = "images/muji.svg";
+    //   img2.src = "images/muji.svg";
        document.getElementById("description-h3").innerHTML = "MUJI";
        document.getElementById("description-h5").innerHTML = "0.38 Gel Pen";
        document.getElementById("description-p").innerHTML = "My absolute must for \"pretty\" notes - the thin weight makes for great study guides."
        description.style.opacity = "1";
        description.style.visibility = "visible";
        description.style.transition = "all 1.5s";
+       mHeader.style.opacity = "0";
+       mHeader.style.transition = "all 0.7s";
      })
 
 // Pilot
@@ -73,13 +85,15 @@ close.addEventListener('click', function() {
  })
  pilot.addEventListener('click', function() {
    img1.src = "images/pilot_line.svg";
-   img2.src = "images/pilot.svg";
+  // img2.src = "images/pilot.svg";
    document.getElementById("description-h3").innerHTML = "PILOT";
    document.getElementById("description-h5").innerHTML = "Precise V5 Retractable Rollerball Pen";
    document.getElementById("description-p").innerHTML = "My ride or die. For the past 5 years, I've used this pen for everyday note-taking!"
    description.style.opacity = "1";
    description.style.visibility = "visible";
    description.style.transition = "all 1.5s";
+   mHeader.style.opacity = "0";
+   mHeader.style.transition = "all 0.7s";
  })
 
 // Prismacolor
@@ -94,13 +108,15 @@ close.addEventListener('click', function() {
 })
 prismacolor.addEventListener('click', function() {
   img1.src = "images/prismacolor_line.svg";
-  img2.src = "images/prismacolor.svg";
+//  img2.src = "images/prismacolor.svg";
   document.getElementById("description-h3").innerHTML = "PRISMACOLOR";
   document.getElementById("description-h5").innerHTML = "Brush Pen";
   document.getElementById("description-p").innerHTML = "I used to use this for drawing, but now I find it useful for brush lettering."
   description.style.opacity = "1";
   description.style.visibility = "visible";
   description.style.transition = "all 1.5s";
+  mHeader.style.opacity = "0";
+  mHeader.style.transition = "all 0.7s";
 })
 
 // Sakura
@@ -115,13 +131,15 @@ sakura.addEventListener('mouseout', function() {
 })
 sakura.addEventListener('click', function() {
   img1.src = "images/sakura_line.svg";
-  img2.src = "images/sakura.svg";
+//  img2.src = "images/sakura.svg";
   document.getElementById("description-h3").innerHTML = "SAKURA";
   document.getElementById("description-h5").innerHTML = "Pigma Sensei 1.0 mm";
   document.getElementById("description-p").innerHTML = "A throwback to those DES 001 days. I love making chunky letters with this marker."
   description.style.opacity = "1";
   description.style.visibility = "visible";
   description.style.transition = "all 1.5s";
+  mHeader.style.opacity = "0";
+  mHeader.style.transition = "all 0.7s";
 })
 
 // Stabilo
@@ -136,13 +154,15 @@ stabilo.addEventListener('mouseout', function() {
 })
 stabilo.addEventListener('click', function() {
   img1.src = "images/stabilo_line.svg";
-  img2.src = "images/stabilo.svg";
+  //img2.src = "images/stabilo.svg";
   document.getElementById("description-h3").innerHTML = "STABILO";
   document.getElementById("description-h5").innerHTML = "Pen 68";
   document.getElementById("description-p").innerHTML = "One of the thicker markers I own. Great for brainstorming on large rolls of paper."
   description.style.opacity = "1";
   description.style.visibility = "visible";
   description.style.transition = "all 1.5s";
+  mHeader.style.opacity = "0";
+  mHeader.style.transition = "all 0.7s";
 })
 
 // Staedtler
@@ -157,13 +177,15 @@ staedtler.addEventListener('mouseout', function() {
 })
 staedtler.addEventListener('click', function() {
   img1.src = "images/staedtler_line.svg";
-  img2.src = "images/staedtler.svg";
+//  img2.src = "images/staedtler.svg";
   document.getElementById("description-h3").innerHTML = "STAEDTLER";
   document.getElementById("description-h5").innerHTML = "Fineliner";
   document.getElementById("description-p").innerHTML = "This pen leaves a fine, gray, unobtrusive mark that I use to add notes to the margins of my study guides and sketches."
   description.style.opacity = "1";
   description.style.visibility = "visible";
   description.style.transition = "all 1.5s";
+  mHeader.style.opacity = "0";
+  mHeader.style.transition = "all 0.7s";
 })
 
 // Tombow
@@ -178,13 +200,15 @@ tombow.addEventListener('mouseout', function() {
 })
 tombow.addEventListener('click', function() {
   img1.src = "images/tombow_line.svg";
-  img2.src = "images/tombow.svg";
+//  img2.src = "images/tombow.svg";
   document.getElementById("description-h3").innerHTML = "TOMBOW";
   document.getElementById("description-h5").innerHTML = "Fudenosuke";
   document.getElementById("description-p").innerHTML = "If I had to choose one pen for brush lettering and fun handwriting, this would be it!"
   description.style.opacity = "1";
   description.style.visibility = "visible";
   description.style.transition = "all 1.5s";
+  mHeader.style.opacity = "0";
+  mHeader.style.transition = "all 0.7s";
 })
 
 
