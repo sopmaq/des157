12b922dc8f24dfbbd41978d348b95f04b1b7d60e
sopmaq <scmaquiling@ucdavis.edu>
2017-02-09 01:04:50 -0800

    add images and transitions

--- studio3/script.js
+++ studio3/script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
    var micron = document.getElementById("micron");
    micron.addEventListener('mouseover', function() {
     micron.style.paddingTop = "50px";
+    micron.style.cursor = "pointer";
+    micron.style.transition = "all 0.7s";
    })
    micron.addEventListener('mouseout', function() {
      micron.style.paddingTop = "0px";
@@ -17,16 +19,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Muji
       var muji = document.getElementById("muji");
       muji.addEventListener('mouseover', function() {
-        muji.style.paddingTop = "50px";
+        muji.style.paddingTop = "70px";
+        muji.style.cursor = "pointer";
+        muji.style.transition = "all 0.7s";
       })
       muji.addEventListener('mouseout', function() {
-        muji.style.paddingTop = "0px";
+        muji.style.paddingTop = "30px";
      })
 
 // Pilot
   var pilot = document.getElementById("pilot");
   pilot.addEventListener('mouseover', function() {
     pilot.style.paddingTop = "100px";
+    pilot.style.cursor = "pointer";
+    pilot.style.transition = "all 0.7s";
   })
   pilot.addEventListener('mouseout', function() {
     pilot.style.paddingTop = "50px";
@@ -35,16 +41,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 // Prismacolor
  var prismacolor = document.getElementById("prismacolor");
  prismacolor.addEventListener('mouseover', function() {
-   prismacolor.style.paddingTop = "50px";
+   prismacolor.style.paddingTop = "55px";
+   prismacolor.style.cursor = "pointer";
+   prismacolor.style.transition = "all 0.7s";
  })
  prismacolor.addEventListener('mouseout', function() {
-   prismacolor.style.paddingTop = "0px";
+   prismacolor.style.paddingTop = "5px";
 })
 
 // Sakura
 var sakura = document.getElementById("sakura");
 sakura.addEventListener('mouseover', function() {
   sakura.style.paddingTop = "50px";
+  sakura.style.cursor = "pointer";
+  sakura.style.transition = "all 0.7s";
 })
 sakura.addEventListener('mouseout', function() {
   sakura.style.paddingTop = "0px";
@@ -53,29 +63,34 @@ sakura.addEventListener('mouseout', function() {
 // Stabilo
 var stabilo = document.getElementById("stabilo");
 stabilo.addEventListener('mouseover', function() {
-  stabilo.style.paddingTop = "50px";
+  stabilo.style.paddingTop = "100px";
+  stabilo.style.cursor = "pointer";
+  stabilo.style.transition = "all 0.7s";
 })
 stabilo.addEventListener('mouseout', function() {
-  stabilo.style.paddingTop = "0px";
+  stabilo.style.paddingTop = "50px";
 })
 
 // Staedtler
 var staedtler = document.getElementById("staedtler");
 staedtler.addEventListener('mouseover', function() {
-  staedtler.style.paddingTop = "50px";
+  staedtler.style.paddingTop = "60px";
+  staedtler.style.cursor = "pointer";
+  staedtler.style.transition = "all 0.7s";
 })
 staedtler.addEventListener('mouseout', function() {
-  staedtler.style.paddingTop = "0px";
+  staedtler.style.paddingTop = "10px";
 })
 
 // Tombow
 var tombow = document.getElementById("tombow");
 tombow.addEventListener('mouseover', function() {
-  tombow.style.paddingTop = "50px";
+  tombow.style.paddingTop = "115px";
   tombow.style.cursor = "pointer";
+  tombow.style.transition = "all 0.7s";
 })
 tombow.addEventListener('mouseout', function() {
-  tombow.style.paddingTop = "0px";
+  tombow.style.paddingTop = "65px";
 })
 
 
