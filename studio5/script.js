ec46f8de5ea4184ec47b05dad8c8012e38f6579d
sopmaq <scmaquiling@ucdavis.edu>
2017-02-22 14:25:26 -0800

    made mic object

--- studio5/script.js
+++ studio5/script.js
@@ -3,22 +3,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    var mic;
-    /* Canvas */
-    function setup() {
-      var canvas = createCanvas(500, 500);
-      canvas.parent('micCanvas');
-
-      // mic = an Audio Input
-      mic = new p5.AudioIn();
-      // connect to speakers
-      mic.start();
-  } // setup()
-
-  function draw() {
-    background(0);
-
-    // create a variable based on respiratory rate
-    var
-  } // draw()
 });
+
+var mic;
+console.log('made mic var');
+
+/* Canvas */
+
+function setup(){
+  console.log('get setup');
+  var canvas = createCanvas(500, 500);
+  canvas.parent('micCanvas');
+
+  // mic = an Audio Input
+  mic = new p5.AudioIn();
+  // connect to speakers
+  mic.start();
+} // setup()
+
+function draw() {
+  console.log('draw function');
+  background(200);
+  // create a variable based on respiratory rate
+} // draw()
