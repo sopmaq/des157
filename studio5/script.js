6b999a12a753bf8e3515cbac91d30f64fdd2fbab
sopmaq <scmaquiling@ucdavis.edu>
2017-02-22 14:45:08 -0800

    create movement based on amplitude

--- studio5/script.js
+++ studio5/script.js
@@ -12,17 +12,36 @@ console.log('made mic var');
 
 function setup(){
   console.log('get setup');
-  var canvas = createCanvas(500, 500);
-  canvas.parent('micCanvas');
+  var canvas1 = createCanvas(200, 200);
+  background(200);
+  canvas1.parent('canvas1');
+/* var canvas2 = createCanvas(200, 200);
+  canvas2.parent('canvas2');
+  var canvas3 = createCanvas(200, 200);
+  canvas3.parent('canvas3');
+  var canvas4 = createCanvas(200, 200);
+  canvas4.parent('canvas4');
+  var canvas5 = createCanvas(200, 200);
+  canvas5.parent('canvas5');
+  var canvas6 = createCanvas(200, 200);
+  canvas6.parent('canvas6');*/
 
   // mic = an Audio Input
   mic = new p5.AudioIn();
   // connect to speakers
   mic.start();
+  // variable based on amplitude
+//  amplitude = new p5.amplitude();
 } // setup()
 
 function draw() {
   console.log('draw function');
   background(200);
-  // create a variable based on respiratory rate
+  // Make circle move based on amplitude
+  // amplitude level is between 0.0 and 1.0
+  var amp = mic.getLevel();
+  fill(127);
+  stroke(0);
+  var h = map(amp, 0, 1, height, 0);
+  ellipse(width/2, h - 25, 50, 50);
 } // draw()
