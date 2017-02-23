cf613641da9d9495e13cc012a2da0f7d606cc5ea
sopmaq <scmaquiling@ucdavis.edu>
2017-02-23 10:21:29 -0800

    add rectangle

--- studio5/script.js
+++ studio5/script.js
@@ -27,22 +27,22 @@ function draw() {
   console.log('draw function');
   background(34, 49, 63);
 
-  // AMPLITUDE
+  // CIRCLE [0][0] - AMPLITUDE
   // Make circle move based on amplitude
   // amplitude level is between 0.0 and 1.0
   // when breathe out: amplitude approaches 1.0
   // when breathe in: amplitude approaches 00
   var amp = mic.getLevel();
-  fill(220, 198, 224);
+  fill(190, 144, 212);
   stroke(255);
   var h = map(amp, 0, 1, 20, 200);
   ellipse(150, 150, h, h)
 
 
-  // AMPLITUDE
+  // CIRCLE [1][0] - AMPLITUDE
   // Change color based on amplitude
   // Circle inside changes radius based
-  // on amplitude 
+  // on amplitude
   var r1 = map(amp, 0, 0.5, 174, 107)
   var g1 = map(amp, 0, 0.5, 168, 185)
   var b1 = map(amp, 0, 0.5, 211, 240)
@@ -56,5 +56,14 @@ function draw() {
   fill(r2, b2, g2);
   stroke(255);
   ellipse(150, 300, h, h)
+
+  // 2 - AMPLITUDE
+  var w = map(amp, 0, 0.5, 0, 100)
+  fill(54, 215, 183);
+  stroke(54, 215, 183);
+  rect(300, 130, w, 30)
+  fill(54, 215, 183);
+  stroke(54, 215, 183);
+  rect(300, 130, -w, 30)
 
 } // draw()
