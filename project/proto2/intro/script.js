a413f8470ede941ab5142440420016b335875ff0
sopmaq <scmaquiling@ucdavis.edu>
2017-03-09 15:25:53 -0800

    update proto2

---
+++ project/proto2/intro/script.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    var introText = document.getElementById("intro-text");
+    
+
+});
